bd5dad83e30f0791ae78c85044e615bf2b79e56c
Tiago Crais <tiagocrais@gmail.com>
2020-11-02 21:25:21 -0300

    Project hr-worker created.

---
+++ hr-worker/src/test/java/com/tiagocr/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagocr.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
